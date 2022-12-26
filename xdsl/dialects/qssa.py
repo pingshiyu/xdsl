2fa3b7de15f81a33ccf73ad4d8cfda6844dca237
pingshiyu <pingshiyu@gmail.com>
2022-12-26 17:30:20 +0000

    splitting pauli into pauli and paulirotations, adding merge/split ops

--- xdsl/dialects/qssa.py
+++ xdsl/dialects/qssa.py
@@ -1,15 +1,18 @@
 # QSSA dialect
 # operations:
-#   - qssa.CNOT
-#   - qssa.{X, Y, Z}
-#   - qssa.{Rx, Ry, Rz}
-#   - qssa.{S, Sdg, T, Tdg}
+#   - qssa.CNOT              # DONE
+#   - qssa.{X, Y, Z}         # DONE
+#   - qssa.{Rx, Ry, Rz}      # DONE
+#   - qssa.S
+#   - qssa.T
+#   - qssa.dag 
 #   - qssa.gate, qssa.U      # for arbitrary gates. U is for a single qubit matrix
-#   - qssa.alloc             # only op with side effects
-#   - qssa.split
+#   - qssa.alloc             # DONE only op with side effects
+#   - qssa.split             
 #   - qssa.concat
 #   - qssa.dim
 #   - qssa.cast
+#   - qssa.H
 # attributes:
 #   - qubit<int, or none (in which case is variadic)>. # this is a type of qubits
 #   - angles (values between 0 and 2pi)                # this could be just a float to parameterise the qssa operation
@@ -39,12 +42,14 @@ from xdsl.irdl import (
     irdl_attr_definition,
     ParameterDef,
     OperandDef,
+    VarOperandDef,
     ResultDef,
     AnyAttr,
     AttributeDef,
     AnyOf,
     BaseAttr,
     builder,
+    attr_constr_coercion
 )
 from xdsl.utils.exceptions import VerifyException, DiagnosticException
 
@@ -60,6 +65,7 @@ from xdsl.dialects.builtin import (
     StringAttr,
     FloatAttr,
 )
+from xdsl.dialects.arith import Constant
 
 from dataclasses import dataclass
 from typing import Union, Optional
@@ -115,13 +121,79 @@ class Alloc(Operation):
     result: Annotated[OpResult, ResultDef(Qubits)]
 
     @staticmethod
-    def allocate(val: Union[int, Attribute]) -> Alloc:
+    def get(val: Union[int, Attribute]) -> Alloc:
         if isinstance(val, int):
             # convert to an IntAttr
             val = IntAttr.from_int(val)
 
         qubits_type = Qubits([val])
         return Alloc.build(result_types=[qubits_type], attributes={"n_qubits": val})
+
+
+@irdl_op_definition
+class Split(Operation):
+    # split into 2 wires - TODO: split into parametric number of wires
+    name: str = "qssa.split"
+
+    # attributes
+    w1_qubits = AttributeDef(BaseAttr(IntAttr)) # qubits in wire 1. TODO: decide if this should be operand or attr?
+
+    # operands
+    w_in: Annotated[SSAValue, OperandDef(BaseAttr(Qubits))]
+
+    # output
+    w1_out: Annotated[OpResult, ResultDef(Qubits)]
+    w2_out: Annotated[OpResult, ResultDef(Qubits)]
+
+    @staticmethod
+    def apply(w_in: Union[Operation, SSAValue], w1_qubits: Union[int, Attribute]) -> Split:
+        if isinstance(w1_qubits, int):
+            # convert to an IntAttr
+            w1_qubits = IntAttr.from_int(w1_qubits)
+
+        # w1 should have type Qubits<w1_qubits.data>
+        # w2 should have type Qubits<n - w1_qubits.data>, where type of input_qubis is Qubits<n>
+        w_in_ssa = SSAValue.get(w_in)
+        w_in_type = w_in_ssa.typ
+        attr_constr_coercion(Qubits).verify(w_in_type)
+
+        w_in_n_qubits = w_in_type.n.data
+        w1_n_qubits = w1_qubits.data
+        w2_n_qubits = w_in_n_qubits - w1_n_qubits
+        if w1_n_qubits <= 0 or w2_n_qubits <= 0:
+            # TODO: use Qubit print method.
+            # TODO: maybe move into verifier of Split
+            raise DiagnosticException(f"Cannot split type {w_in_n_qubits} wire into type {(w1_n_qubits, )} wires")
+
+        w1_type = Qubits([IntAttr(w1_n_qubits)])
+        w2_type = Qubits([IntAttr(w2_n_qubits)])
+        return Split.build(operands=[w_in_ssa], result_types=[w1_type, w2_type], attributes={"w1_qubits": w1_qubits})
+
+
+@irdl_op_definition
+class Merge(Operation):
+    # merge several wires together
+    name: str = "qssa.merge"
+
+    # attributes
+
+    # operands
+    ws_in: Annotated[list[SSAValue], VarOperandDef(Qubits)]
+
+    # output
+    w_out: Annotated[OpResult, ResultDef(Qubits)]
+
+    @staticmethod
+    def _verify_and_get_n(w_in: Union[Operation, SSAValue]) -> int:
+        w_in_typ = SSAValue.get(w_in).typ
+        attr_constr_coercion(Qubits).verify(w_in_typ)
+        return w_in_typ.n.data
+
+    @staticmethod
+    def apply(ws_in: list[Union[Operation, SSAValue]]) -> Merge:
+        n_total = sum(Merge._verify_and_get_n(w_in) for w_in in ws_in)
+        wn_type = Qubits([IntAttr(n_total)])
+        return Merge.build(operands=[ws_in], result_types=[wn_type])
 
 
 @irdl_op_definition
@@ -141,10 +213,37 @@ class CNOT(Operation):
 
 @irdl_op_definition
 class Pauli(Operation):
-    name: str = "qssa.Pauli"
+    name: str = "qssa.pauli"
 
     # attributes
     # TODO: add constraint on the "X", "Y", "Z" strs
+    pauli_type = AttributeDef(BaseAttr(StringAttr))
+
+    # inputs
+    input: Annotated[SSAValue, OperandDef(q1_Type)]
+
+    # outputs
+    out: Annotated[OpResult, ResultDef(q1_Type)]
+
+    @staticmethod
+    def apply(
+        input_qubits: Union[Operation, SSAValue], pauli_type: str
+    ) -> Pauli:
+        return Pauli.build(
+            operands=[input_qubits],
+            result_types=[q1_Type],
+            attributes={
+                "pauli_type": StringAttr.from_str(pauli_type),
+            },
+        )
+
+
+@irdl_op_definition
+class PauliRotate(Operation):
+    name: str = "qssa.pauli_rotate"
+
+    # attributes
+    # TODO: add constraint on the "Rx", "Ry", "Rz" strs
     # TODO: custom printing of the angles
     pauli_type = AttributeDef(BaseAttr(StringAttr))
     rotation = AttributeDef(BaseAttr(FloatAttr)) # in multiples of pi
@@ -158,8 +257,8 @@ class Pauli(Operation):
     @staticmethod
     def apply(
         input_qubits: Union[Operation, SSAValue], pauli_type: str, rotation: float = 0.0
-    ) -> Pauli:
-        return Pauli.build(
+    ) -> PauliRotate:
+        return PauliRotate.build(
             operands=[input_qubits],
             result_types=[q1_Type],
             attributes={
@@ -169,7 +268,7 @@ class Pauli(Operation):
         )
 
 
-Quantum = Dialect([Alloc, CNOT, Pauli], [Qubits, Angle])
+Quantum = Dialect([Alloc, CNOT, PauliRotate, Pauli, Split], [Qubits, Angle])
 
 
 def _show_indent(
@@ -202,17 +301,17 @@ def main() -> None:
 
     # Test1: check we can alloc qubits
     print("1: Qubit allocation...")
-    q10 = Alloc.allocate(10)
-    _show_indent(printer, "q5", q10)
+    q10 = Alloc.get(10)
+    _show_indent(printer, "q10", q10)
 
     # manually specify the result type and operation params
-    q5_ = Alloc.build(result_types=[q5_type], attributes={"n_qubits": int_attr_5})
-    _show_indent(printer, "q5_", q5_)
+    q5 = Alloc.build(result_types=[q5_type], attributes={"n_qubits": int_attr_5})
+    _show_indent(printer, "q5", q5)
 
     # Test2: check CNOT gate
     # this should be applied
     print("2: CNOT application")
-    q2 = Alloc.allocate(2)
+    q2 = Alloc.get(2)
     _show_indent(printer, "qssa.alloc q2", q2)
 
     cnot_00 = CNOT.apply(q2)
@@ -220,7 +319,7 @@ def main() -> None:
     _show_indent(printer, "qssa.cnot(q2)", cnot_00)
 
     # this should not be applied (wrong input type)
-    q3 = Alloc.allocate(3)
+    q3 = Alloc.get(3)
     _show_indent(printer, "qssa.alloc q3", q3)
 
     cnot_000 = CNOT.apply(q3)
@@ -234,23 +333,23 @@ def main() -> None:
         )
 
     # Test3: check Pauli gates
-    print("3: Pauli application")
+    print("3: Pauli/PauliRotate application")
     
     # pauli apply (raw pauli)
-    q1 = Alloc.allocate(1)
+    q1 = Alloc.get(1)
     _show_indent(printer, "qssa.alloc q1", q1)
 
-    pauli_x0_0 = Pauli.apply(q1, "X")
+    pauli_x0_0 = PauliRotate.apply(q1, "X")
     _show_indent(printer, "qssa.pauli {'X', 0.0} q1", pauli_x0_0)
     pauli_x0_0.verify()
 
     # pauli apply (with angles)
-    pauli_zhalf_0 = Pauli.apply(q1, "Z", 0.50)
+    pauli_zhalf_0 = PauliRotate.apply(q1, "Z", 0.50)
     _show_indent(printer, "qssa.pauli {'Z', 0.50} q1", pauli_zhalf_0)
     pauli_zhalf_0.verify()
 
     # pauli apply on wrong qubit type
-    pauli_y0_00 = Pauli.apply(q2, "Y", 1.00)
+    pauli_y0_00 = PauliRotate.apply(q2, "Y", 1.00)
     _show_indent(printer, "qssa.pauli {'Y', 1.00} q2", pauli_y0_00)
     try:
         pauli_y0_00.verify()
@@ -259,6 +358,46 @@ def main() -> None:
             printer, "qssa.pauli {'Y', 1.00} q2 successfully caught!", str(e).split("\n")[0]
         )
 
+    # Test 4: split/merge
+    print("4: Wire split and merge")
+
+    # normal split: 5 -> 3, 2
+    q23_ssa = Split.apply(q5, 3)
+    q2_ssa, q3_ssa = q23_ssa.results
+    _show_indent(printer, "qssa.split {3} q5", q23_ssa)
+
+    # error split: 5 -> 5, 0
+    try:
+        q50_s = Split.apply(q5, 5)
+    except DiagnosticException as e:
+        _show_indent(printer, "qssa.split {5} q5 successfully caught!", str(e))
+
+    # error split: 5 -> -1, 6
+    try:
+        qneg16_s = Split.apply(q5, -1)
+    except DiagnosticException as e:
+        _show_indent(printer, "qssa.split {-1} q5 successfully caught!", str(e))
+
+    # error split: wrong input type, not Qubits<n> type.
+    i64_3 = Constant.from_int_and_width(3, 64)
+    try:
+        split_i64_3 = Split.apply(i64_3, 2)
+    except DiagnosticException as e:
+        _show_indent(printer, "qssa.split (arith.constant 3) error caught!", str(e))
+
+    # merge: merge 3, 5 -> 8
+    q8 = Merge.apply([q3, q5])
+    _show_indent(printer, "qssa.merge (q3, q5)", q8)
+    
+    # merge: merge splitted wires 2, 3 -> 5
+    q5_merged = Merge.apply([q2_ssa, q3_ssa])
+    _show_indent(printer, "qssa.merge (q2, q3)", q5_merged)
+
+    # merge: type mismatch
+    try:
+        Merge.apply([i64_3, q8])
+    except DiagnosticException as e:
+        _show_indent(printer, "qssa.merge (arith.constant 3, q8) error caught!", str(e))
 
 
 if __name__ == "__main__":
