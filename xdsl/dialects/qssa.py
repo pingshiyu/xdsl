739fb824b158195f927da11b1ca89db6bc98797b
pingshiyu <pingshiyu@gmail.com>
2022-12-26 21:57:26 +0000

    adding dim, attmpt at adding gate type, H, S, T gate operations, tests for dim operation

--- xdsl/dialects/qssa.py
+++ xdsl/dialects/qssa.py
@@ -1,18 +1,22 @@
 # QSSA dialect
 # operations:
+#   - qssa.alloc             # DONE only op with side effects
+#   - qssa.split             # DONE
+#   - qssa.merge             # DONE
+#   - qssa.dim               # DONE
+#   - qssa.cast              #
+#   - qssa.measure           # 
+#   - qssa.gate, qssa.U      # for arbitrary gates. U is for a single qubit matrix (by describing angles)
 #   - qssa.CNOT              # DONE
 #   - qssa.{X, Y, Z}         # DONE
 #   - qssa.{Rx, Ry, Rz}      # DONE
-#   - qssa.S
-#   - qssa.T
-#   - qssa.dag 
-#   - qssa.gate, qssa.U      # for arbitrary gates. U is for a single qubit matrix
-#   - qssa.alloc             # DONE only op with side effects
-#   - qssa.split             
-#   - qssa.concat
-#   - qssa.dim
-#   - qssa.cast
-#   - qssa.H
+#   - qssa.S                 # DONE
+#   - qssa.T                 # DONE
+#   - qssa.H                 # DONE
+#   - qssa.dag               # DONE but problem: this is actually an operation on operations => needs to map Gate Ops into "dagged" Gate Ops!
+#       - two options for modelling this:
+#           - 1. introduce new type "Gate" which is what gate ops will now return, and an "apply" operation to apply Gates on Qubits
+#           - 2. leave as an identity operation on Qubits, but encode its semantics in the rewrite rules <-- using this for now
 # attributes:
 #   - qubit<int, or none (in which case is variadic)>. # this is a type of qubits
 #   - angles (values between 0 and 2pi)                # this could be just a float to parameterise the qssa operation
@@ -46,34 +50,32 @@ from xdsl.irdl import (
     ResultDef,
     AnyAttr,
     AttributeDef,
+    OptAttributeDef,
     AnyOf,
     BaseAttr,
     builder,
-    attr_constr_coercion
+    attr_constr_coercion,
 )
-from xdsl.utils.exceptions import VerifyException, DiagnosticException
+from xdsl.utils.exceptions import DiagnosticException
 
 # other dialects
 from xdsl.dialects.scf import Scf
 from xdsl.dialects.builtin import (
     Builtin,
     IntAttr,
-    Annotated,
     i32,
-    IntegerAttr,
-    IntegerType,
+    Annotated,
     StringAttr,
     FloatAttr,
 )
 from xdsl.dialects.arith import Constant
 
-from dataclasses import dataclass
 from typing import Union, Optional
 
 # type attributes. Actually these are type functions: Qubits :: [Attribute] -> Attribute
 @irdl_attr_definition
 class Qubits(ParametrizedAttribute):
-    name = "qubit"
+    name = "qubits"
 
     # number of qubits
     n: ParameterDef[IntAttr]
@@ -86,6 +88,7 @@ class Qubits(ParametrizedAttribute):
 
 q1_Type = Qubits.get_n_qubits_type(1)
 q2_Type = Qubits.get_n_qubits_type(2)
+
 
 # data attributes
 @irdl_attr_definition
@@ -131,12 +134,32 @@ class Alloc(Operation):
 
 
 @irdl_op_definition
+class Dim(Operation):
+    name: str = "qssa.dim"
+
+    # operands
+    input_qubit: Annotated[SSAValue, OperandDef(Qubits)]
+
+    # output
+    output_qubit: Annotated[SSAValue, ResultDef(Qubits)]
+    dimension: Annotated[SSAValue, ResultDef(i32)]
+
+    @staticmethod
+    def apply(input_qubit: Union[Operation, SSAValue]) -> Dim:
+        input_type = SSAValue.get(input_qubit).typ
+        attr_constr_coercion(Qubits).verify(input_type)
+        return Dim.build(operands=[input_qubit], result_types=[input_type, i32])
+
+
+@irdl_op_definition
 class Split(Operation):
     # split into 2 wires - TODO: split into parametric number of wires
     name: str = "qssa.split"
 
     # attributes
-    w1_qubits = AttributeDef(BaseAttr(IntAttr)) # qubits in wire 1. TODO: decide if this should be operand or attr?
+    w1_qubits = AttributeDef(
+        BaseAttr(IntAttr)
+    )  # qubits in wire 1. TODO: decide if this should be operand or attr?
 
     # operands
     w_in: Annotated[SSAValue, OperandDef(BaseAttr(Qubits))]
@@ -146,7 +169,9 @@ class Split(Operation):
     w2_out: Annotated[OpResult, ResultDef(Qubits)]
 
     @staticmethod
-    def apply(w_in: Union[Operation, SSAValue], w1_qubits: Union[int, Attribute]) -> Split:
+    def apply(
+        w_in: Union[Operation, SSAValue], w1_qubits: Union[int, Attribute]
+    ) -> Split:
         if isinstance(w1_qubits, int):
             # convert to an IntAttr
             w1_qubits = IntAttr.from_int(w1_qubits)
@@ -163,11 +188,17 @@ class Split(Operation):
         if w1_n_qubits <= 0 or w2_n_qubits <= 0:
             # TODO: use Qubit print method.
             # TODO: maybe move into verifier of Split
-            raise DiagnosticException(f"Cannot split type {w_in_n_qubits} wire into type {(w1_n_qubits, )} wires")
+            raise DiagnosticException(
+                f"Cannot split type {w_in_n_qubits} wire into type {(w1_n_qubits, )} wires"
+            )
 
         w1_type = Qubits([IntAttr(w1_n_qubits)])
         w2_type = Qubits([IntAttr(w2_n_qubits)])
-        return Split.build(operands=[w_in_ssa], result_types=[w1_type, w2_type], attributes={"w1_qubits": w1_qubits})
+        return Split.build(
+            operands=[w_in_ssa],
+            result_types=[w1_type, w2_type],
+            attributes={"w1_qubits": w1_qubits},
+        )
 
 
 @irdl_op_definition
@@ -194,6 +225,60 @@ class Merge(Operation):
         n_total = sum(Merge._verify_and_get_n(w_in) for w_in in ws_in)
         wn_type = Qubits([IntAttr(n_total)])
         return Merge.build(operands=[ws_in], result_types=[wn_type])
+
+
+# class Dagged(Enum):
+#     NONE = 0
+#     DAGGED = auto()
+#
+#
+# @irdl_attr_definition
+# class DaggedAttr(Data[Dagged]):
+#     name = "dagged"
+#
+#     @staticmethod
+#     def parse_parameter(parser: Parser) -> Dagged:
+#         data = parser.parse_str_literal()
+#         if (data.lower() == "dagged"):
+#             return Dagged.DAGGED
+#         else:
+#             return Dagged.NONE
+#
+#     @staticmethod
+#     def print_parameter(data: Dagged, printer: Printer) -> None:
+#         printer.print_string("dagged" if data == Dagged.DAGGED else "none")
+#
+#     @staticmethod
+#     @builder
+#     def from_bool(data: bool) -> DaggedAttr:
+#         return DaggedAttr(data)
+
+
+@irdl_op_definition
+class Dag(Operation):
+    name: str = "qssa.dag"
+
+    # operands
+    qubits_in: Annotated[SSAValue, OperandDef(Qubits)]
+
+    # output
+    qubits_out: Annotated[OpResult, ResultDef(Qubits)]
+
+    @staticmethod
+    def apply(input_qubits: Union[Operation, SSAValue]) -> Dag:
+        return Dag.build(
+            operands=[input_qubits], result_types=[SSAValue.get(input_qubits).typ]
+        )
+
+
+# class Gate(Operation, ABC):
+#     @property
+#     @abstractmethod
+#     def name() -> str:
+#         return "qssa.gate"
+#
+#     # attributes
+#     dagged = OptAttributeDef(BaseAttr(DaggedAttr))
 
 
 @irdl_op_definition
@@ -226,9 +311,7 @@ class Pauli(Operation):
     out: Annotated[OpResult, ResultDef(q1_Type)]
 
     @staticmethod
-    def apply(
-        input_qubits: Union[Operation, SSAValue], pauli_type: str
-    ) -> Pauli:
+    def apply(input_qubits: Union[Operation, SSAValue], pauli_type: str) -> Pauli:
         return Pauli.build(
             operands=[input_qubits],
             result_types=[q1_Type],
@@ -246,7 +329,7 @@ class PauliRotate(Operation):
     # TODO: add constraint on the "Rx", "Ry", "Rz" strs
     # TODO: custom printing of the angles
     pauli_type = AttributeDef(BaseAttr(StringAttr))
-    rotation = AttributeDef(BaseAttr(FloatAttr)) # in multiples of pi
+    rotation = AttributeDef(BaseAttr(FloatAttr))  # in multiples of pi
 
     # inputs
     input: Annotated[SSAValue, OperandDef(q1_Type)]
@@ -266,6 +349,51 @@ class PauliRotate(Operation):
                 "rotation": FloatAttr.from_value(rotation),
             },
         )
+
+
+@irdl_op_definition
+class SGate(Operation):
+    name: str = "qssa.s"
+
+    # inputs
+    input: Annotated[SSAValue, OperandDef(q1_Type)]
+
+    # outputs
+    out: Annotated[OpResult, ResultDef(q1_Type)]
+
+    @staticmethod
+    def apply(input_qubit: Union[Operation, SSAValue]) -> SGate:
+        return SGate.build(operands=[input_qubit], result_types=[q1_Type])
+
+
+@irdl_op_definition
+class HGate(Operation):
+    name: str = "qssa.h"
+
+    # inputs
+    input: Annotated[SSAValue, OperandDef(q1_Type)]
+
+    # outputs
+    out: Annotated[OpResult, ResultDef(q1_Type)]
+
+    @staticmethod
+    def apply(input_qubit: Union[Operation, SSAValue]) -> HGate:
+        return HGate.build(operands=[input_qubit], result_types=[q1_Type])
+
+
+@irdl_op_definition
+class TGate(Operation):
+    name: str = "qssa.t"
+
+    # inputs
+    input: Annotated[SSAValue, OperandDef(q1_Type)]
+
+    # outputs
+    out: Annotated[OpResult, ResultDef(q1_Type)]
+
+    @staticmethod
+    def apply(input_qubit: Union[Operation, SSAValue]) -> TGate:
+        return TGate.build(operands=[input_qubit], result_types=[q1_Type])
 
 
 Quantum = Dialect([Alloc, CNOT, PauliRotate, Pauli, Split], [Qubits, Angle])
@@ -334,7 +462,7 @@ def main() -> None:
 
     # Test3: check Pauli gates
     print("3: Pauli/PauliRotate application")
-    
+
     # pauli apply (raw pauli)
     q1 = Alloc.get(1)
     _show_indent(printer, "qssa.alloc q1", q1)
@@ -355,7 +483,9 @@ def main() -> None:
         pauli_y0_00.verify()
     except DiagnosticException as e:
         _show_indent(
-            printer, "qssa.pauli {'Y', 1.00} q2 successfully caught!", str(e).split("\n")[0]
+            printer,
+            "qssa.pauli {'Y', 1.00} q2 successfully caught!",
+            str(e).split("\n")[0],
         )
 
     # Test 4: split/merge
@@ -388,7 +518,7 @@ def main() -> None:
     # merge: merge 3, 5 -> 8
     q8 = Merge.apply([q3, q5])
     _show_indent(printer, "qssa.merge (q3, q5)", q8)
-    
+
     # merge: merge splitted wires 2, 3 -> 5
     q5_merged = Merge.apply([q2_ssa, q3_ssa])
     _show_indent(printer, "qssa.merge (q2, q3)", q5_merged)
@@ -398,6 +528,14 @@ def main() -> None:
         Merge.apply([i64_3, q8])
     except DiagnosticException as e:
         _show_indent(printer, "qssa.merge (arith.constant 3, q8) error caught!", str(e))
+
+    # Test 5: dimension
+    print("5: Dimensions operation")
+
+    # normal split: 5 -> 3, 2
+    q5_dim_ssa = Dim.apply(q5)
+    q5_ssa, i32_5_ssa = q5_dim_ssa.results
+    _show_indent(printer, "qssa.dim q5", q5_dim_ssa)
 
 
 if __name__ == "__main__":
